1fba1b39c735b0425501601f4540ec31029f050f
Majobustos31 <maria.bustos-l@mail.escuelaing.edu.co>
2026-08-23 18:41:15 +0000

    Clase SlotMachine

---
+++ Bases_proyecto/SlotMachine.java
@@ -0,0 +1,92 @@
+import java.util.ArrayList;
+
+public class SlotMachine
+{
+    private ArrayList<Wheel> wheels;
+    private boolean visible;
+
+    public SlotMachine()
+    {
+        
+    }
+
+    public void addWheel(int pos)
+    {
+        
+    }
+
+    public void delWheel(int pos)
+    {
+        
+    }
+
+    public void addSymbol(int pos, String color)
+    {
+        
+    }
+
+    public void delSymbol(String symbol)
+    {
+        
+    }
+
+    public void placeSymbol(int wheel, String symbol)
+    {
+        
+    }
+
+    public void spin(int wheel)
+    {
+        
+    }
+
+    public void spin()
+    {
+        
+    }
+
+    public String[] symbols()
+    {
+        return null;
+    }
+
+    public int distinctSymbols()
+    {
+        return 0;
+    }
+
+    public String[] configuration()
+    {
+        return null;
+    }
+
+    public boolean isJackpot()
+    {
+        return false;
+    }
+
+    public void makeVisible()
+    {
+        
+    }
+
+    public void makeInvisible()
+    {
+        
+    }
+
+    public void exit()
+    {
+        
+    }
+
+    public boolean ok()
+    {
+        return false;
+    }
+
+    private void showError(String message)
+    {
+        
+    }
+}
