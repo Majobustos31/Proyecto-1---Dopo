bcc677b8dc283e305acf3d4431bd13e33062642a
Majobustos31 <maria.bustos-l@mail.escuelaing.edu.co>
2026-08-23 22:49:06 +0000

    Clase Wheel

---
+++ Bases_proyecto/Wheel.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class Wheel
+{
+    private ArrayList<Symbol> symbols;
+    private int position;
+
+    public Wheel()
+    {
+        
+    }
+
+    public void addSymbol(Symbol symbol)
+    {
+        
+    }
+
+    public boolean removeSymbol(String name)
+    {
+        return false;
+    }
+
+    public void spin(int steps)
+    {
+        
+    }
+
+    public Symbol getVisibleSymbol()
+    {
+        return null;
+    }
+
+    private int findSymbol(String name)
+    {
+        return -1;
+    }
+
+    public int getNumberSymbols()
+    {
+        return 0;
+    }
+}
