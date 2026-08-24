896515df413cd8471d7307fe4e1ea92056116d10
Majobustos31 <maria.bustos-l@mail.escuelaing.edu.co>
2026-08-24 01:14:25 +0000

    Clase Symbol

---
+++ Bases_proyecto/Symbol.java
@@ -0,0 +1,30 @@
+public class Symbol
+{
+    private String name;
+    private String color;
+
+    public Symbol(String name, String color)
+    {
+        
+    }
+
+    public String getName()
+    {
+        return "";
+    }
+
+    public String getColor()
+    {
+        return "";
+    }
+
+    public void makeVisible()
+    {
+        
+    }
+
+    public void makeInvisible()
+    {
+        
+    }
+}
